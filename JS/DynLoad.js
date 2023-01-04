9b1a5162dd44d261a9374385742eefe57c736b6d
Manu Srivastava <92544857+ManuSrivastava1@users.noreply.github.com>
2023-01-04 16:10:17 +0100

    Add files via upload

--- JS/DynLoad.js
+++ JS/DynLoad.js
@@ -1,26 +1,4 @@
-//console.log("string test for succesful file load")
-//localStorage.setItem('href','CSS/blankcss.css');
-
-//console.log(localStorage)
-
-String.prototype.tpl = function(o) 
-{ 
-    var r = this ; 
-    console.log(r)
-
-    for (var i in o) { 
-        //console.log(i) //this is URL first time and Label the second time
-        r = r.replace(new RegExp("\\$"+i, 'g'),o[i])  
-        //console.log(o[i])  //this is the name of the label
-        //console.log(r)
-    } 
-    //console.log(r)
-    return r 
-}
-var listItemTpl = `load("$url")`
-//var listItemTpl = `<button class="dropdown-item nav__link" type="button" href='#' onclick='load("$url")'>$label</button>`
-//var listItemTpl = `<li><button class="dropdown-item nav__link" type="button" href='#' onclick='load("$url")'>$label</button></li>`
-
+  // CSS SELECTOR SCRIPT 
 function swapTheme(csspath) {
     //console.log("i get called")
     document.getElementById('choosen').setAttribute('href', csspath);
@@ -80,252 +58,303 @@ function persi()
     }
     }
 
+  // CHECKBOX METADATA SCRIPT 
+  document.addEventListener('DOMContentLoaded', function () {
+    var buttons = document.querySelectorAll('.dropdown-item');
+    for (var i = 0; i < buttons.length; i++) {
+      buttons[i].addEventListener('click', function () {
+        // get the selected article
+        var selectedArticle = this.value;
 
-$(document).ready(main);
+        // create an XMLHttpRequest object
+        var xhttp = new XMLHttpRequest();
+
+        // specify the function to be called when the response is received
+        xhttp.onreadystatechange = function () {
+          if (this.readyState == 4 && this.status == 200) {
+            // insert the contents of the HTML file into the page
+            document.getElementById('article').innerHTML = this.responseText;
+            const checkboxes = document.querySelectorAll('#checkboxContainer input[type="checkbox"]');
+            checkboxes.forEach(checkbox => {
+              checkbox.checked = false;
+            });
+            document.getElementById('listContainer').innerHTML = '';
+          }
+        };
+
+        // open the HTML file and send the request
+        xhttp.open('GET', 'articles/' + selectedArticle + '.html', true);
+        xhttp.send();
+      });
+    }
+  });
 
 
-
-function main() {
-    //console.log("Does this gets called first?")
-    $.ajax({
-        method: 'GET',
-        url: 'FILES.json',
-        success: function(f) {
-            //console.log(f)
-            for (var i=0; i<f.length; i++) {
-                $('#list').append(listItemTpl.tpl({url:f[i].url}))
-                //console.log("some")
-            }	
-            for (var i=0; i<f.length; i++){
-                var elementDiv = document.getElementById(i)
-                console.log(elementDiv)
-                
-                var button = document.createElement("button");
-                button.setAttribute("type", "button");
-                var data = listItemTpl.tpl({url:f[i].url, label: f[i].label})
-                button.setAttribute('onclick', data)
-                //button.addEventListener("click", load)
-                  
-                // Set the content of the button
-                button.innerHTML = "Read This Article";
-
-                // Add the button to the document
-                elementDiv.appendChild(button);
-                console.log(i)
-            }
-        },
-        error: function() {
-            alert('No document to show')
-        }
+document.addEventListener('DOMContentLoaded', function () {
+    // Add event listeners to the buttons
+    document.getElementById('button1').addEventListener('click', function () {
+      loadArticle('article1');
     });
-}
-    
-/*   
-    $('#showasides').click(function() {
-        if (this.checked) 
-            $('.aside').addClass('text-aside')
-        else
-            $('.aside').removeClass('text-aside')
-    })
-    $('#showkeywords').click(function() {
-        if (this.checked) 
-            $('.keyword').addClass('text-keyword')
-        else
-            $('.keyword').removeClass('text-keyword')
-    })
-    $('#showspeeches').click(function() {
-        if (this.checked) 
-            $('q.speech').addClass('text-speeches')
-        else
-            $('q.speech').removeClass('text-speeches')
-    })
-    $('#showquotes').change(function() {
-        if (this.checked) 
-            $('q:not(.speech)').addClass('text-quotes')
-        else
-            $('q:not(speech)').removeClass('text-quotes')
-    })
-
-    $('#createasides').click(function() {
-        var s = window.getSelection().toString()
-        addToDoc(s,'aside','span')
-        addToLocalStorage(s,'aside')
-    })
-    $('#createkeywords').click(function() {
-        var s = window.getSelection().toString()
-        if (s.length > 3) {
-            addToDoc(s,'keyword','span')
-            addToLocalStorage(s,'keyword')
-        }
-    })
-    $('#createspeeches').click(function() {
-        var s = window.getSelection().toString()
-        addToDoc(s,'speech','q')
-        addToLocalStorage(s,'speech')
-    })
-    $('#createquotes').change(function() {
-        var s = window.getSelection().toString()
-        addToDoc(s,'quote','q')
-        addToLocalStorage(s,'quote')
-    })
-} */
-
-/*
-function addToLocalStorage(string, type) {
-    var ls = getFromLocalStorage()
-    if (ls[type].indexOf(string)==-1) ls[type].push(string)
-    localStorage.setItem('fvSentences',JSON.stringify(ls))		
-}
-
-function getFromLocalStorage() {
-    var lsString = localStorage.getItem('fvSentences') 
-    var ls = JSON.parse(lsString)
-    if (ls==undefined)  {
-        ls = {
-            aside: [],
-            keyword: [],
-            speech: [],
-            quote: []
-        }
-    }
-    return ls
-}
-
-function addFromLocalStorage() {
-    var ls = getFromLocalStorage()
-    for (var i in ls) {
-        for (var k in ls[i]) {
-            addToDoc(ls[i][k], i , 'span')
-        }
-    }
-}
-
-function addToDoc(string,type,tag) {
-    var replacement = `<${tag} class='${type}'>${string}</${tag}>`
-    if (isMoreThanOneWord(string)) {
-        var variations = new RegExp(string, 'mgi')  
-            // g: global: replace multiple occurrence and not just the first
-            // i: ignore case: replace matching string regardless of the case 
-    } else {
-        var variations = findAllVariations(string)
-    }
-    // bad bad bad bad solution: complicated, fragile, computational intensive. 
-    // Can you think of something better?
-    var text = $('#file').html().replace(variations,replacement)
-    $('#file').html(text)
-    addIds()
-    filltabs()
-}
-
-function isMoreThanOneWord(s) {
-    return s.match(/\s/)   // there is at least one whitespace character in the string
-}
-
-function findAllVariations(s) {
-    var desinences = "[s|es|ing|ed|d]?" // optional additional desinences
-    
-    return new RegExp(`\\b${s}${desinences}\\b`,"gi")   
-}
-*/
-
-function load(file) {
-    //this gets called when 
-    $.ajax({
-        method: 'GET',
-        url: file,
-        success: function(d) {
-            console.log("1111")
-            $('#file').html(d)
-            $('.show').prop("checked", false)
-            /*
-            addIds()
-            fillInfo("#file", "#info")
-            filltabs()
-            addFromLocalStorage()
-            $('#title').html($('#file h1')) */
-
-            // Get the element with an id of "heading" - need to add "author" & "date published"
-            var element = document.getElementById("heading"); //heading
-
-            // Get the content of the element
-            var content = element.innerHTML;
-            console.log(content);
-            // Output the content to the inner HTML
-            var elements = document.getElementById("TITLE");
-
-            // Modify the content of the element
-            elements.innerHTML = content;
-        },
-        error: function() {
-            alert('Could not load file '+file)
-        }
+    document.getElementById('button2').addEventListener('click', function () {
+      loadArticle('article2');
     });
-}
+    document.getElementById('button3').addEventListener('click', function () {
+      loadArticle('article3');
+    });
+  });
+  function loadArticle(article) {
+  // create an XMLHttpRequest object
+  var xhttp = new XMLHttpRequest();
 
-/*
-function addIds() {
-    addId('.aside','aside')
-    addId('.keyword','keyword')
-    addId('q.speech', 'speech')
-    addId('q:not(.speech)', 'quote')
-}
-
-function addId(what, prefix) {
-    var id = '0'
-    var elements = $(what); 
-    for (var i=0; i<elements.length; i++) {
-        elements[i].id = prefix + "-" + id++
+  // specify the function to be called when the response is received
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      // insert the contents of the HTML file into the page
+      document.getElementById('article').innerHTML = this.responseText;
     }
+  };
+
+  // open the HTML file and send the request
+  xhttp.open('GET', 'articles/' + article + '.html', true);
+  xhttp.send();
 }
-function filltabs(){
-    filltab("#file .aside","list-aside","#asides")
-    filltab("#file .keyword","list-keyword","#keywords")
-    filltab("#file q.speech","list-speech","#speeches")
-    filltab("#file q:not(.speech)","list-quote","#quotes")
+  
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  // Get the buttons
+  var button1 = document.getElementById('button1');
+  var button2 = document.getElementById('button2');
+  var button3 = document.getElementById('button3');
+
+  // Add the click event listener to button1
+  button1.addEventListener('click', function () {
+    // Clear the checkboxes and the list elements
+    clearCheckboxesAndList();
+
+    // Get the selected article
+    var selectedArticle = this.dataset.article;
+
+    // Create an XMLHttpRequest object
+    var xhttp = new XMLHttpRequest();
+
+    // Specify the function to be called when the response is received
+    xhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        // Insert the contents of the HTML file into the page
+        document.getElementById('article').innerHTML = this.responseText;
+      }
+    };
+
+    // Open the HTML file and send the request
+    xhttp.open('GET', 'articles/' + selectedArticle + '.html', true);
+    xhttp.send();
+  });
+
+  // Add the click event listener to button2
+  button2.addEventListener('click', function () {
+    // Clear the checkboxes and the list elements
+    clearCheckboxesAndList();
+
+    // Get the selected article
+    var selectedArticle = this.dataset.article;
+
+    // Create an XMLHttpRequest object
+    var xhttp = new XMLHttpRequest();
+
+    // Specify the function to be called when the response is received
+    xhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        // Insert the contents of the HTML file into the page
+        document.getElementById('article').innerHTML = this.responseText;
+      }
+    };
+
+    // Open the HTML file and send the request
+    xhttp.open('GET', 'articles/' + selectedArticle + '.html', true);
+    xhttp.send();
+  });
+
+  // Add the click event listener to button3
+  button3.addEventListener('click', function () {
+    // Clear the checkboxes and the list elements
+    clearCheckboxesAndList();
+
+    // Get the selected article
+    var selectedArticle = this.dataset.article;
+
+    // Create an XMLHttpRequest object
+    var xhttp = new XMLHttpRequest();
+
+    // Specify the function to be called when the response is received
+    xhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        // Insert the contents of the HTML file into the page
+        document.getElementById('article').innerHTML = this.responseText;
+      }
+    };
+
+    // Open the HTML file and send the request
+    xhttp.open('GET', 'articles/' + selectedArticle + '.html', true);
+    xhttp.send();
+  }
+  )})
+
+  function clearCheckboxesAndList() {
+  // Uncheck all the checkboxes
+  const checkboxes = document.querySelectorAll('#checkboxContainer input[type="checkbox"]');
+  checkboxes.forEach(checkbox => {
+    checkbox.checked = false;
+  });
+
+  // Clear the list
+  document.getElementById('listContainer').innerHTML = '';
 }
 
-function fillInfo(from, where) {
-    var item = `
-        <p class="list title"><b>Title: </b> $title</p>
-        <p class="list author"><b>Author: </b> $author</p>
-        <p class="list author"><b>Headings: </b><ul>$headings</ul></p>
-        ` ;
-    $(where).empty(); 
 
-    var title = $(from + ' h1')[0].innerText
-    var author = $(from + ' .byline')[0].innerText
-    var headingList = $(from + ' h2')
-    var headings = ""
-    for (var i=0; i<headingList.length; i++) {
-        headings += "<li class='small'>"+headingList[i].innerHTML+"</li>"
+
+function updateList(selectedCheckbox) {
+  // Clear the list
+  document.getElementById('listContainer').innerHTML = '';
+
+  // Get all the checkboxes
+  const checkboxes = document.querySelectorAll('#checkboxContainer input[type="checkbox"]');
+
+  // Uncheck all the checkboxes except the one that was selected
+  checkboxes.forEach(checkbox => {
+    if (checkbox !== selectedCheckbox) {
+      checkbox.checked = false;
     }
-    $(where).append(item.tpl( {
-        author: author,
-        title: title,
-        headings: headings
-    }))
+  });
+
+  // Get all the checked checkboxes
+  const checkedBoxes = document.querySelectorAll('#checkboxContainer input:checked');
+  let classes = [];
+  checkedBoxes.forEach(box => {
+    classes.push(box.id);
+  });
+
+  // If no checkboxes are checked, return
+  if (classes.length === 0) {
+    return;
+  }
+
+  // Get all the elements with the selected classes
+  const elements = document.querySelectorAll(classes.map(className => '.' + className).join(','));
+  elements.forEach(element => {
+    // Create a new list item for each element
+    const item = document.createElement('li');
+    item.innerHTML = element.innerHTML;
+    item.onclick = () => {
+      // Remove the "highlighted" class from all the elements
+      const highlighted = document.querySelectorAll('.highlighted');
+      highlighted.forEach(el => {
+        el.classList.remove('highlighted');
+        el.style.backgroundColor = '';
+      });
+
+      // Highlight the element and navigate to it when the list item is clicked
+      element.style.backgroundColor = 'green';
+      element.classList.add('highlighted');
+      window.location.href = '#' + element.id;
+    };
+    document.getElementById('listContainer').appendChild(item);
+  });
 }
 
-function filltab(what,style,where) {
-    var list = `<li class="list $style"><a href="#" onclick="goto('$place')">$content</a></li>`
-    var elements = $(what); 
-    $(where+' ul').empty(); 
-    for (var i=0; i<elements.length; i++) {
-        $(where+' ul').append(list.tpl({
-            style:style, 
-            place: '#'+elements[i].id,
-            content: elements[i].innerHTML
-        }) )
-    }
-}
 
-function goto(id) {
-    var t = $(id)[0].offsetTop;
-    $('body').animate({ scrollTop: t }, 200);
-    $(id).addClass('animate');
-    setTimeout(function(){
-        $(id).removeClass('animate');
-    },5000);
-}
-*/
+document.addEventListener('DOMContentLoaded', function () {
+    // Get the value of the "article" query string parameter
+    var urlParams = new URLSearchParams(window.location.search);
+    var selectedArticle = urlParams.get('article');
 
+    // Load the selected article
+    loadArticle(selectedArticle);
+  });
+
+  function loadArticle(article) {
+    // Create an XMLHttpRequest object
+    var xhttp = new XMLHttpRequest();
+
+    // Specify the function to be called when the response is received
+    xhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        // Insert the contents of the HTML file into the page
+        document.getElementById('article').innerHTML = this.responseText;
+      }
+    };
+
+    // Open the HTML file and send the request
+    xhttp.open('GET', 'articles/' + article + '.html', true);
+    xhttp.send();
+  }
+
+
+  setInterval(function(){
+ var text = [];
+ $('.heading').each(function(){
+     text.push( $(this).text());
+ });
+ var myString = text.join(', ');
+ $('#heading').text(myString);},300);
+
+  setInterval(function(){
+ var text = [];
+ $('.date').each(function(){
+     text.push( $(this).text());
+ });
+ var myString = text.join(', ');
+ $('#date').text(myString);},300);
+
+  setInterval(function(){
+ var text = [];
+ $('.author').each(function(){
+     text.push( $(this).text());
+ });
+ var myString = text.join(', ');
+ $('#author').text(myString);},300);
+
+  setInterval( function myFunction() {
+    var number = document.querySelector("meta[name='description']").getAttribute("content");
+    document.getElementById("metamd").innerHTML = number;
+  }, 300);
+
+
+
+
+    // DROPDOWN TO LOADED ARTICLE FROM OTHER PAGES SCRIPT
+// Navbar dropdown
+document.getElementById('navbarDropdown').addEventListener('click', function (event) {
+    // Get the selected article
+    var selectedArticle = event.target.dataset.article;
+  
+    // Redirect the user to the original page with the selected article as a query string parameter
+    window.location.href = '/issue.html?article=' + selectedArticle;
+  });
+  
+  // Original page
+  document.addEventListener('DOMContentLoaded', function () {
+    // Get the value of the "article" query string parameter
+    var urlParams = new URLSearchParams(window.location.search);
+    var selectedArticle = urlParams.get('article');
+  
+    // Load the selected article
+    loadArticle(selectedArticle);
+  });
+  
+  function loadArticle(article) {
+    // Create an XMLHttpRequest object
+    var xhttp = new XMLHttpRequest();
+  
+    // Specify the function to be called when the response is received
+    xhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        // Insert the contents of the HTML file into the page
+        document.getElementById('article').innerHTML = this.responseText;
+      }
+    };
+  
+    // Open the HTML file and send the request
+    xhttp.open('GET', 'articles/' + article + '.html', true);
+    xhttp.send();
+  }
